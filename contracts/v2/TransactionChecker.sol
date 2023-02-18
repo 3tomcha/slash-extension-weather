825646e75834f6acdf70e1eca0aed80c44788220
Tomoya Kobayashi <tomocha.marika@gmail.com>
2023-02-18 20:20:36 +0900

    Create TransactionChecker.sol

---
+++ contracts/v2/TransactionChecker.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.8.0;
+
+contract TransactionChecker {
+    
+    mapping(address => uint256[]) public userTransactions;
+    mapping(address => uint256) public userAverage;
+    
+    function isTransactionAmountValid(address _user, uint256 _transactionAmount) public view returns(bool) {
+        if (_transactionAmount > (userAverage[_user] * 10)) {
+            return true;
+        }
+        return false;
+    }
+    
+    function addTransaction(address _user, uint256 _transactionAmount) public {
+        userTransactions[_user].push(_transactionAmount);
+        uint256 sum = 0;
+        for (uint256 i = 0; i < userTransactions[_user].length; i++) {
+            sum += userTransactions[_user][i];
+        }
+        userAverage[_user] = sum / userTransactions[_user].length;
+    }
+}
